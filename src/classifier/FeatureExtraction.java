6a3612dcf95460c74abf78853c33234a43b28cfd
Leonos <covic416@gmail.com>
2022-08-18 15:42:03 -0400

    Fit the new image to the old image

--- src/classifier/FeatureExtraction.java
+++ src/classifier/FeatureExtraction.java
@@ -19,7 +19,7 @@ public class FeatureExtraction
 		Mat img=matrix.clone(); //Clones, Mat is matrix of colors
 		
 		Size sz = new Size(64, 128);
-		Imgproc.resize(img, matrix, sz); //resizes the matrix
+		Imgproc.resize(matrix, img, sz); //resizes the matrix
 		
 		Mat tmp=img.clone(); //Clone the matrix, i presume to prevent messing up the original
 		
@@ -30,20 +30,20 @@ public class FeatureExtraction
 		
 		int binNum=9; //0-180
 		int stepSize=180/binNum;
+		int count=0;
 		
 		for(int x=1; x<img.cols()-2; x++)
 		{
 			for(int y=1; y<img.rows()-2; y++)
 			{
-				//System.out.println(y);
 				Gx[x][y]=img.get(y, x+1)[0]-img.get(y, x-1)[0];
 				Gy[x][y]=img.get(y-1, x)[0]-img.get(y+1, x)[0];
 				
 				magnitude[x][y]=Math.sqrt(Math.pow(Gx[x][y], 2)+Math.pow(Gy[x][y], 2));
 				theta[x][y]=Math.atan(Gy[x][y]/Gx[x][y]);
 				
-				tmp.put(x, y, Gx[x][y]); //puts a new pixel in location x, y
-				System.out.println(Gx[x].length);
+				count++;
+				tmp.put(y, x, Gx[x][y]); //puts a new pixel in location x, y
 			}
 		}
 		
